30d3102745c80308005f4a58e646a7e4d0fc57c9
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-06 23:38:02 +0200

    tick rate 60

--- pygame.py
+++ pygame.py
@@ -3,10 +3,12 @@ from sys import exit
 pygame.init()
 
 Screen = pygame.display.set_mode((800, 400))
-
+pygame.display.set_caption('Game one')
+clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     pygame.display.update()
+    clock.tick(60)
