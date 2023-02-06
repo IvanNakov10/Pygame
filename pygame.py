2bc94f4e86ca2217acb383d01c36cfffb48589e1
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-06 23:54:32 +0200

    font + ground

--- pygame.py
+++ pygame.py
@@ -6,14 +6,17 @@ Screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game one')
 clock = pygame.time.Clock()
 surface = pygame.image.load('Pygame/graphics/Sky.png')
+ground = pygame.image.load('Pygame/graphics/ground.png')
+font = pygame.font.Font(None, 50)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
-    Screen.blit(surface,(0,0))
 
+    Screen.blit(surface,(0,0))
+    Screen.blit(ground,(0,300))
+    
     pygame.display.update()
     clock.tick(60)
 
