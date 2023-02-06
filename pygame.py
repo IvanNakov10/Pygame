0eece6984a89f3a949e1682dfba00848ad5c0003
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-07 00:08:09 +0200

    basic snail

--- pygame.py
+++ pygame.py
@@ -9,6 +9,8 @@ surface = pygame.image.load('Pygame/graphics/Sky.png')
 ground = pygame.image.load('Pygame/graphics/ground.png')
 font = pygame.font.Font('Pygame/font/Pixeltype.ttf', 50)
 text_surface = font.render('My game', False, 'White')
+snail = pygame.image.load('Pygame/graphics/snail/snail1.png')
+snail_x_pos = 600
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -18,6 +20,8 @@ while True:
     Screen.blit(surface,(0,0))
     Screen.blit(ground,(0,300))
     Screen.blit(text_surface,(300,50))
+    snail_x_pos -= 4
+    Screen.blit(snail,(snail_x_pos,250))
     pygame.display.update()
     clock.tick(60)
 
