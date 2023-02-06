bb6b79eee55cea86773039c14df8ab2c9f900921
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-06 23:47:14 +0200

    audio, graphics. fonts

--- pygame.py
+++ pygame.py
@@ -5,10 +5,16 @@ pygame.init()
 Screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game one')
 clock = pygame.time.Clock()
+surface = pygame.Surface((100, 150))
+surface.fill('Yellow')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    
+    Screen.blit(surface,(0,0))
+
     pygame.display.update()
     clock.tick(60)
+
