ecfdaedd5a859ef1ab75adc2abff8216142cead9
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-06 23:35:14 +0200

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -1,2 +1,12 @@
 import pygame
+from sys import exit
 pygame.init()
+
+Screen = pygame.display.set_mode((800, 400))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
