a8d9dcdf470e3365757b416d8381247ae92b3264
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-07 00:12:48 +0200

    .convert() imgs

--- pygame.py
+++ pygame.py
@@ -5,11 +5,11 @@ pygame.init()
 Screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game one')
 clock = pygame.time.Clock()
-surface = pygame.image.load('Pygame/graphics/Sky.png')
-ground = pygame.image.load('Pygame/graphics/ground.png')
+surface = pygame.image.load('Pygame/graphics/Sky.png').convert()
+ground = pygame.image.load('Pygame/graphics/ground.png').convert()
 font = pygame.font.Font('Pygame/font/Pixeltype.ttf', 50)
 text_surface = font.render('My game', False, 'White')
-snail = pygame.image.load('Pygame/graphics/snail/snail1.png')
+snail = pygame.image.load('Pygame/graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
 while True:
     for event in pygame.event.get():
